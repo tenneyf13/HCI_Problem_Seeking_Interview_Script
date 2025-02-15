ac233e6a253bebe2cebbd50be3285a6dbe538cbe
tenneyf13 <70172174+tenneyf13@users.noreply.github.com>
2025-02-15 23:17:33 +0000

    code base

---
+++ src/components/app/page.js
@@ -0,0 +1,9 @@
+import AdaptiveInterviewGuide from '@/components/FrontEndScript'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <AdaptiveInterviewGuide />
+    </main>
+  )
+}
